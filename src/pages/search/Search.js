1e55979600156bd72cbdfeaf449524f1da032434
pn-coding <148750503+pn-coding@users.noreply.github.com>
2024-07-23 17:36:24 +0900

    search

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -3,6 +3,10 @@ import { FiSearch } from "react-icons/fi";
 import styled from "styled-components";
 import { spacing } from "../../GlobalStyled";
 import { searchMovie } from "../../api";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Loading } from "../../components/Loading";
+import { W500_URL } from "../../constant/imgUrl";
 
 const Container = styled.div`
   padding: 150px ${spacing.side};
@@ -39,7 +43,26 @@ const ErrorMessage = styled.h4`
   color: gold;
 `;
 
+const ConWrap = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  row-gap: 30px;
+  column-gap: 15px;
+`;
+
+const Con = styled.div``;
+
+const Bg = styled.div`
+  height: 400px;
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
 export const Search = () => {
+  const [searchData, setSearchData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const {
     register,
     handleSubmit,
@@ -49,9 +72,12 @@ export const Search = () => {
   const onSearchResult = async (data) => {
     const { keyword } = data;
 
-    const result = await searchMovie(keyword);
-    console.log(result);
+    const { results } = await searchMovie(keyword);
+    setSearchData(results);
+    setIsLoading(false);
   };
+
+  console.log(searchData?.length === 0 ? "없음" : "있음");
 
   return (
     <Container>
@@ -69,6 +95,35 @@ export const Search = () => {
 
         <ErrorMessage>{errors?.keyword?.message}</ErrorMessage>
       </Form>
+
+      {searchData?.length === 0 ? (
+        "검색결과없음"
+      ) : (
+        <>
+          {searchData && (
+            <ConWrap>
+              {isLoading ? (
+                <Loading />
+              ) : (
+                <>
+                  {searchData.map((data) => (
+                    <Con key={data.id}>
+                      <Link to={`/detail/${data.id}`}>
+                        <Bg>
+                          <img
+                            src={W500_URL + data.poster_path}
+                            alt={data.title}
+                          />
+                        </Bg>
+                      </Link>
+                    </Con>
+                  ))}
+                </>
+              )}
+            </ConWrap>
+          )}
+        </>
+      )}
     </Container>
   );
 };
